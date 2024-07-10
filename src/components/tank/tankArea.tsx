55ee2920536e18503650a1c02ff41f2db32a6296
Dominic Petriella <dompetriella@gmail.com>
2024-07-09 21:28:08 -0400

    boyfriend - added tank area

---
+++ src/components/tank/tankArea.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+export default function TankArea() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <div>
+        </div>
+    </>
+  )
+}
+
